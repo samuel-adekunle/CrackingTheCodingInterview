7958d8d74d35989c63d13077e3664a5278184473
Samuel Adekunle <me@samuel-adekunle.software>
2021-08-28 00:06:51 +0100

    have a great day

---
+++ 1/1.1/main.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <unordered_set>
+
+/**
+ * Determine if a string has all unique characters
+ * 
+ * @param str string to be checked
+ * @return whether or not 'str' has all unique characters
+*/
+bool isUnique(const std::string &str)
+{
+	std::unordered_set<char> mem;
+	for (const char &c : str)
+	{
+		if (mem.contains(c))
+			return false;
+
+		mem.insert(c);
+	}
+	return true;
+}
+
+int main(int argc, char *argv[])
+{
+	assert(argc > 1);		   // Expected 1 command line argument
+	return !isUnique(argv[1]); // 0 is True, 1 is False
+}
